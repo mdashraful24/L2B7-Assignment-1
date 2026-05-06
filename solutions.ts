dd9f2d1bcbc72779af82257cc132a90e2f5535c4
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-06 23:48:42 +0600

    added problem 2 solution

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,10 @@
 function filterEvenNumbers(numbers: number[]): number[] {
     return numbers.filter(num => num % 2 === 0);
 }
+
+
+// Problem 2 solution: Reverse a String
+
+function reverseString(str: string): string {
+    return str.split("").reverse().join("");
+}
